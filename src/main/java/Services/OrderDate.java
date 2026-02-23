ebf9bbf3a24b68e7758f45a34b517d0d71c7a159
DanielKaczorowsk <d.kaczorowski.1999@gmail.com>
2026-02-23 20:32:28 +0100

    Updated order structur and i created a prototype,register and login, but there is still room for improvement

--- src/main/java/Services/OrderDate.java
+++ src/main/java/Services/OrderDate.java
@@ -2,12 +2,14 @@ package Services;
 
 import Model.Operation.ProductsOperation.OrderOperation;
 import Model.Operation.ProductsOperation.OrderOperationInterface;
+import Model.Operation.ProductsOperation.UserOperation;
 
 import java.util.function.Supplier;
 
 public enum OrderDate
 {
     ORDER_DATE(OrderOperation::new),
+    USER_DATE(UserOperation::new),
     ;
     private final Supplier<OrderOperationInterface<?>> supplier;
     OrderDate(Supplier<OrderOperationInterface<?>> supplier)
