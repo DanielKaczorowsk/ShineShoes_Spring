fa9549fa03b265b5cf43f695f5e6f8c7f6c5fc33
DanielKaczorowsk <d.kaczorowski.1999@gmail.com>
2026-02-23 20:49:23 +0100

    Przywrócenie wszystkich brakujących plików

--- src/main/java/Services/OrderDate.java
+++ src/main/java/Services/OrderDate.java
@@ -2,14 +2,12 @@ package Services;
 
 import Model.Operation.ProductsOperation.OrderOperation;
 import Model.Operation.ProductsOperation.OrderOperationInterface;
-import Model.Operation.ProductsOperation.UserOperation;
 
 import java.util.function.Supplier;
 
 public enum OrderDate
 {
     ORDER_DATE(OrderOperation::new),
-    USER_DATE(UserOperation::new),
     ;
     private final Supplier<OrderOperationInterface<?>> supplier;
     OrderDate(Supplier<OrderOperationInterface<?>> supplier)
